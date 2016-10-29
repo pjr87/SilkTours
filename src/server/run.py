b991ce05814f75bfd60615c3b51a600ccfccbdbd
AndrewShidel <andrew@shidel.com>
2016-10-29 12:04:34 -0400

    Adds very basic python server

---
+++ src/server/run.py
@@ -0,0 +1,22 @@
+# Run a test server.
+#from app import app
+#app.run(host='0.0.0.0', port=8000, debug=True)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/users/<username>', methods=['GET'])
+def get_user(username):
+    return 'User %s' % username
+
+@app.route('/users/<username>', methods=['POST'])
+def set_user(username):
+    return 'Creating new User %s' % username
+
+if __name__ == "__main__":
+    app.run()
+
