fd923eed32e51181c6b0f9cdde62c9d897897e6f
AndrewShidel <andrew@shidel.com>
2016-11-02 21:08:30 -0400

    Adds creating/editing of users. Better data protection.

--- src/server/user.py
+++ src/server/user.py
@@ -1,29 +1,92 @@
-class User:
-    props = {}
-    PROPS = ["name", "profile_picture", "intrests", "location", "tours_taking", "tours_completed"
-             "phone", "description", "dob"];
+import boto3
+import uuid
 
-    def __init__(self, id):
-        self.props["id"] = id;
-        self.props["name"] = "Andrew Shidel"
-        self.props["profile_picture"] = "https://s3.amazon.com/silktours/somebucket/someimage.jpg"
-        self.props["intrests"] = ["Sports", "Beer", "Nature"]
-        self.props["location"] = "Philadelphia"
-        self.props["tours_taking"] = ["tour_id_1", "tour_id_2"]
-        self.props["tours_completed"] = ["tour_id_3", "tour_id_3"]
-        self.props["phone"] = "412-651-0498"
-        self.props["description"] = "This is a user."
-        self.props["dob"] = "11/11/1991"
+
+class User:
+    # A set of all properties
+    PROPS = {"name", "profilePicture", "intrests", "location", "tours_taking",
+             "tours_completed", "phone", "description", "dob", "password"}
+
+    # A set of properties required to create a new user
+    REQUIRED_PROPS = {"name", "description"}  # TODO Determine required props
+
+    # A set of properties visible to consumer of the API
+    VISABLE_PROPS = {"name", "profilePicture", "location", "phone",
+                     "description", "dob", "address", "city", "country",
+                     "zip"}
+
+    def __init__(self):
+        self.props = {}
+        self.dynamodb = boto3.resource('dynamodb')
+        self.table = self.dynamodb.Table('User')
+        self.changed = set()
+
+    def getById(self, id):
+        response = self.table.get_item(
+            Key={
+                'u_id': id
+            }
+        )
+
+        if "Item" not in response:
+            return
+        self.parseResponse(response)
+
+    def parseResponse(self, response):
+        if "Item" not in response:
+            return
+
+        response = response["Item"]
+        for item in response:
+            item_local = item[2:]
+            if item_local in User.VISABLE_PROPS:
+                self.props[item_local] = response[item]
+
+    def create(self, props):
+        for prop in User.REQUIRED_PROPS:
+            if prop not in props:
+                raise Exception('Missing property: ' + prop)
+                return
+        self.setProps(props)
+        response = self.table.put_item(
+            Item=self.prepareProps()
+        )
+        if "Item" in response:
+            self.parseResponse(response["Item"])
+
+    def prepareProps(self):
+        result = {}
+        visableProps = set(self.props.keys()) & User.VISABLE_PROPS
+        for prop in visableProps:
+            result["u_" + prop] = self.props[prop]
+        result["u_id"] = uuid.uuid4().hex
+        return result
 
     def setProps(self, props):
         for key in props:
-            if key not in self.PROPS:
-                raise Exception('Invalid key: ' + key)
-            self.props[key] = props[key]
+            if key not in User.VISABLE_PROPS:
+                raise Exception('Invalid property: ' + key)
+            if key not in self.props or (self.props[key] != props[key]):
+                self.props[key] = props[key]
+                self.changed.add(key)
 
     def serialize(self):
         return self.props
 
     def commit(self):
-        # TODO
-        print ("Saving " + name + " in the database...")
+        if len(self.changed) == 0:
+            return
+
+        updateExpression = "SET "
+        attributeValues = {}
+        for changedItem in self.changed:
+            updateExpression += "u_" + changedItem + " = :" + changedItem + ','
+            attributeValues[':'+changedItem] = self.props[changedItem]
+        updateExpression = updateExpression[:-1]
+
+        self.table.update_item(
+            Key={'u_id': self.props["id"]},
+            UpdateExpression=updateExpression,
+            ExpressionAttributeValues=attributeValues
+        )
+        self.changed = set()
