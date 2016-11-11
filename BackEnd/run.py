83ba5a7a753c7bffeab43b5c665d02211dc37587
AndrewShidel <andrew@shidel.com>
2016-11-11 14:15:20 -0500

    Moves src/server/app to Backend

--- BackEnd/run.py
+++ BackEnd/run.py
@@ -1,13 +1,13 @@
-# Run a test server.
-# from app import app
-# app.run(host='0.0.0.0', port=8000, debug=True)
-
 from flask import Flask
 from flask import jsonify
 from flask import request
 from user import User
 import boto3
 
+from app.database_module.controlers import dyanamoDbController
+from app.s3_module.controlers import S3Controller
+from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -35,6 +35,8 @@ def notAuthorizedResponse():
     return "<h1>403: Not Authorized. Click <a"
     + " href='http://localhost:5000/login'>here</a> to login.</h1>", 403
 
+db = dyanamoDbController()
+s3 = S3Controller()
 
 @app.route("/")
 def hello():
@@ -70,9 +72,40 @@ def edit_user(id):
     return jsonify(user.serialize())
 
 
-@app.route('/users/<id>', methods=['DELETE'])
-def delete_user(id):
-    return 'Deleting new User %s' % id
+@app.route('/tours', methods=['GET'])
+def get_tour_list():
+    return db.list_tours()
+
+
+@app.route('/tours/<tourid>', methods=['GET'])
+def get_tour(tourid):
+    return db.list_tour_with_id(tourid)
+
+
+@app.route('/tours/<tourname>', methods=['POST'])
+def set_tour(tourname):
+    return db.post_tour_with_name(tourname)
+
+
+@app.route('/tours/<tourid>', methods=['PUT'])
+def edit_tour(tourid):
+    return db.edit_tour_with_id(tourid)
+
+
+@app.route('/pics', methods=['GET'])
+def get_tour_pics():
+    return s3.getItems()
+
+
+@app.route('/ratings/<ratingid>', methods=['GET'])
+def get_rating(ratingid):
+    return db.list_rating_with_id(ratingid)
+
+
+@app.route('/tourevents/<teid>', methods=['GET'])
+def get_tourevent(teid):
+    return db.list_tourevent_with_id(teid)
+
 
 if __name__ == "__main__":
     app.run()
