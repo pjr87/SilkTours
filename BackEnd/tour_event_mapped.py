0412c61b76c4b9646cb2c791d7902f01cf4b8258
AndrewShidel <andrew@shidel.com>
2017-01-11 12:25:33 -0500

    Adds tour details to tours_taking and tours_teaching.

--- BackEnd/tour_event_mapped.py
+++ BackEnd/tour_event_mapped.py
@@ -1,5 +1,6 @@
 import datetime
 from interests_mapped import Interests
+from tour_mapped import Tour
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
@@ -11,10 +12,13 @@ class TourEvent(Base):
     id_tourEvent = Column(Integer, primary_key=True)
     id_guide = Column(Integer, ForeignKey("User.id_users"))
     id_user = Column(Integer, ForeignKey("User.id_users"))
-    #id_tour = Column(Integer, ForeignKey("Tour.id_tour"))
+    id_tour = Column(Integer, ForeignKey("Tour.id_tour"))
     #id_rating = Column(Integer, ForeignKey("Ratings.id_rating"))
+    tour = relationship("Tour", foreign_keys=[id_tour])
 
     def serialize(self):
+        print self.tour
         return {
-            "id_tourEvent": self.id_tourEvent
+            "id_tourEvent": self.id_tourEvent,
+            "tour": self.tour.serialize(False)
         }
--- BackEnd/tour_mapped.py
+++ BackEnd/tour_mapped.py
@@ -1,12 +1,43 @@
 import datetime
 from interests_mapped import Interests
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Date, Boolean
+from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 from base import Base
 
 
-class User(Base):
-    __tablename__ = 'User'
-    id_users = Column(Integer, primary_key=True)
+class Tour(Base):
+    __tablename__ = 'Tour'
+    id_tour = Column(Integer, primary_key=True)
+    firstStart_date = Column(Date)
+    lastEnd_date = Column(Date)
+    name = Column(String)
+    price = Column(String)
+    description = Column(String)
+    min_group_size = Column(Integer)
+    max_group_size = Column(Integer)
+    address_zip = Column(String)
+    address_street = Column(String)
+    address_suffix = Column(String)
+    address_unit = Column(String)
+    address_unit_number = Column(String)
     address_city = Column(String)
+    address_country = Column(String)
+    profile_image = Column(String)
+    id_guide = Column(Integer, ForeignKey("User.id_users"))
+    is_deleted = Column(Boolean)
+    additional_accomadation = Column(String)
+    additional_food = Column(String)
+    additional_transport = Column(String)
+    id_rating = Column(Integer)
+
+    def serialize(self, deep):
+        result = {}
+
+        for c in self.__table__.columns:
+            key = c.name
+            value = getattr(self, c.name)
+            if type(value) is datetime.date:
+                value = str(value)
+            result[key] = value
+        return result
