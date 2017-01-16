c5d4dea98fa989190ed664963280420e50f78a43
Troy Santry <Troy@Troys-MacBook-Air.local>
2017-01-16 18:04:54 -0500

    added a settings page and a dropdown menu for when the user is signed in

---
+++ FrontEnd/src/app/components/Dropdown.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import style from './style/style.css';
+import { BrowserRouter as Router, Link, Match, Miss, Redirect } from 'react-router';
+
+
+class AccountDropdown extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      droppedDown:false
+    };
+  }
+
+  handleLinkClick() {
+    this.setState({droppedDown:false});
+  }
+
+  handleOnClick(){
+      this.setState({droppedDown:!this.state.droppedDown});
+  }
+  render(){
+    if(this.state.droppedDown){
+      var output = (
+        <div className={style.dropdown}>
+          <a onClick={this.handleOnClick.bind(this)}>
+            <div> {this.props.name}</div>
+          </a>
+          <div className={style.dropdownContent}>
+            <Link to='/profile'>profile</Link>
+            <Link to='/settings'>settings</Link>
+            <a>logout</a>
+          </div>
+        </div>);
+    }
+    else{
+      var output = (  <div className={style.dropdown}>
+          <a onClick={this.handleOnClick.bind(this)}> <div > {this.props.name}</div> </a>
+        </div>
+        );
+    }
+    return output;
+  }
+}
+
+export default AccountDropdown;
--- FrontEnd/src/app/index.js
+++ FrontEnd/src/app/index.js
@@ -4,6 +4,8 @@ import ExplorePage from './components/ExplorePage';
 import Activities from './components/Activities';
 import AvailableToursPage from './components/AvailableToursPage';
 import Header from './components/Header';
+import Settings from './components/Settings'
+import Profile from './components/Profile'
 import Sign from './components/Sign';
 import About from './components/About';
 import { BrowserRouter as Router, Route, Link, Match, Miss, browserHistory } from 'react-router';
@@ -15,7 +17,8 @@ ReactDOM.render((<Router history={browserHistory}>
       <Match pattern='/activities' component={Activities}/>
       <Match pattern='/about' component={About}/>
       <Match pattern='/sign' component={Sign}/>
-      <Match pattern='/sign' component={Sign}/>
+      <Match pattern='/profile' component={Profile}/>
+      <Match pattern='/settings' component={Settings}/>
       <Match pattern='/explore' component={AvailableToursPage}/>
     </div>
   </Router>), rootElement);
