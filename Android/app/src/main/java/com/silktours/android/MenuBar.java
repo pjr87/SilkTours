34871fbf00d512d2cb7dac331aab01712bbd12f3
AndrewShidel <andrew@shidel.com>
2017-01-25 17:02:15 -0500

    Menu Bar labels now update when user navigates to a page

--- Android/app/src/main/java/com/silktours/android/MenuBar.java
+++ Android/app/src/main/java/com/silktours/android/MenuBar.java
@@ -13,6 +13,17 @@ public class MenuBar {
     public static void setupClickListeners(final Activity activity) {
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
                 activity.findViewById(R.id.bottom_navigation);
+        Class<?> activityClass = activity.getClass();
+        if (activityClass == Profile.class) {
+            bottomNavigationView.getMenu().getItem(0).setChecked(true);
+        }else if (activityClass == MessageThreads.class) {
+            bottomNavigationView.getMenu().getItem(2).setChecked(true);
+        }else if (activityClass == MyTours.class) {
+            bottomNavigationView.getMenu().getItem(3).setChecked(true);
+        }else{
+            bottomNavigationView.getMenu().getItem(1).setChecked(true);
+        }
+
 
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -26,10 +37,10 @@ public class MenuBar {
                                 startActivity(activity, search.class);
                                 break;
                             case R.id.action_messages:
-                                startActivity(activity, Messages.class);
+                                startActivity(activity, MessageThreads.class);
                                 break;
                             case R.id.action_my_tours:
-                                startActivity(activity, MessageThreads.class);
+                                startActivity(activity, MyTours.class);
                                 break;
                         }
                         return false;
