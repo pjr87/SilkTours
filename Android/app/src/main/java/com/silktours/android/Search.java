6d2ac9244f7b4a306e1fb407166902649e08ec0a
AndrewShidel <andrew@shidel.com>
2017-02-05 15:57:29 -0500

    Tours search results now layout better.

--- Android/app/src/main/java/com/silktours/android/Search.java
+++ Android/app/src/main/java/com/silktours/android/Search.java
@@ -64,6 +64,12 @@ public class Search extends Fragment {
 
         recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerView);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
+        makeLayoutManager();
+        recyclerView.setLayoutManager(manager);
+        return rootView;
+    }
+
+    private void makeLayoutManager() {
         if(getActivity().getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
             manager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         }
@@ -71,11 +77,7 @@ public class Search extends Fragment {
             manager = new StaggeredGridLayoutManager(4, StaggeredGridLayoutManager.VERTICAL);
         }
         manager.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
-
         manager.setItemPrefetchEnabled(false);
-        recyclerView.setLayoutManager(manager);
-
-        return rootView;
     }
 
     private void launchFilterDialog() {
@@ -131,6 +133,7 @@ public class Search extends Fragment {
 
     private void search() {
         filterParams.query = searchView.getQuery().toString();
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -147,9 +150,13 @@ public class Search extends Fragment {
                 MainActivity.getInstance().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-
-                        RecyclerView.Adapter i = new SearchResultsAdapter(MainActivity.getInstance(), tours);
-                        recyclerView.setAdapter(i);
+                        RecyclerView.Adapter i = new SearchResultsAdapter(
+                                MainActivity.getInstance(),
+                                tours,
+                                recyclerView.getWidth(),
+                                recyclerView.getHeight()
+                        );
+                        recyclerView.swapAdapter(i, false);
                     }
                 });
             }
