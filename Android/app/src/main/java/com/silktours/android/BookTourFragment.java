1f7ba2f2baa9517c2aec76d014a595a1ae4b0eee
AndrewShidel <andrew@shidel.com>
2017-02-23 15:46:04 -0500

    Adds selecting of dates in booktour

--- Android/app/src/main/java/com/silktours/android/BookTourFragment.java
+++ Android/app/src/main/java/com/silktours/android/BookTourFragment.java
@@ -12,8 +12,10 @@ import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidListener;
 import com.silktours.android.database.Tour;
 import com.silktours.android.database.TourEvent;
 import com.silktours.android.database.User;
@@ -25,7 +27,10 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Created by andrew on 2/23/17.
@@ -35,7 +40,8 @@ public class BookTourFragment extends Fragment {
     private User user;
     private View rootView;
     private CaldroidFragment caldroidFragment;
-
+    SimpleDateFormat dateFormatter=new SimpleDateFormat("yyyy-MM-dd");
+    private Map<String, List<TourEvent>> eventMap = new HashMap<>();
 
     public static void start(Tour tour, User user) {
         Bundle args = new Bundle();
@@ -58,6 +64,7 @@ public class BookTourFragment extends Fragment {
 
     private void setupCalendar() {
         this.caldroidFragment = new CaldroidFragment();
+        caldroidFragment.setCaldroidListener(listener);
         Bundle args = new Bundle();
         Calendar cal = Calendar.getInstance();
         args.putInt(CaldroidFragment.MONTH, cal.get(Calendar.MONTH) + 1);
@@ -69,6 +76,40 @@ public class BookTourFragment extends Fragment {
 
         new GetEvents().execute(1);
     }
+
+    final CaldroidListener listener = new CaldroidListener() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        @Override
+        public void onSelectDate(Date date, View view) {
+            if (eventMap.containsKey(dateFormatter.format(date))) {
+                Toast.makeText(MainActivity.getInstance().getApplicationContext(), formatter.format(date),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onChangeMonth(int month, int year) {
+            String text = "month: " + month + " year: " + year;
+            Toast.makeText(MainActivity.getInstance().getApplicationContext(), text,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onLongClickDate(Date date, View view) {
+            Toast.makeText(MainActivity.getInstance().getApplicationContext(),
+                    "Long click " + formatter.format(date),
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCaldroidViewCreated() {
+            Toast.makeText(MainActivity.getInstance().getApplicationContext(),
+                    "Caldroid view is created",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
 
     private class GetEvents extends AsyncTask<Integer, Integer, List<TourEvent>> {
         protected List<TourEvent> doInBackground(Integer... id) {
@@ -91,6 +132,12 @@ public class BookTourFragment extends Fragment {
                 try {
                     Date startDate = parser.parse(result.getStr("start_date_time"));
                     caldroidFragment.setBackgroundDrawableForDate(blue, startDate);
+                    String date = dateFormatter.format(startDate);
+                    List<TourEvent> eventList = eventMap.get(date);
+                    if (eventList == null) {
+                        eventMap.put(date, eventList);
+                    }
+                    eventList.add(result);
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
