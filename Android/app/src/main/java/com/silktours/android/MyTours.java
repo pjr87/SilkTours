ea9a58c42994dac1de748f0092e7b87eba92e3c3
Yongqiang Fan <yf322@drexel.edu>
2017-04-08 12:06:10 -0400

    add spinner to mytours

--- Android/app/src/main/java/com/silktours/android/MyTours.java
+++ Android/app/src/main/java/com/silktours/android/MyTours.java
@@ -9,8 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import com.silktours.android.database.Controller;
 import com.silktours.android.database.Tours;
@@ -32,6 +34,7 @@ public class MyTours extends Fragment {
     private ListView listView;
     private LayoutInflater inflater;
     private Bundle bundle;
+    private Spinner spinner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,6 +43,12 @@ public class MyTours extends Fragment {
         currentID = R.id.DefaultFrame;
         this.inflater = inflater;
         listView = (ListView) rootView.findViewById(R.id.tourListView);
+        spinner = (Spinner) rootView.findViewById(R.id.spnRole);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.getInstance(),
+                R.array.spinner_items, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new itemOnSelectedListener());
         getTours();
         return rootView;
     }
@@ -48,7 +57,6 @@ public class MyTours extends Fragment {
         new AsyncTask<Void, Void, String>() {
             @Override
             protected String doInBackground(Void... params) {
-
                 try {
                     return Controller.sendGet("/search");
                 } catch (Exception e) {
@@ -77,6 +85,10 @@ public class MyTours extends Fragment {
         }.execute();
     }
 
+    public void getGuidedTours() {
+        
+    }
+
     private void startFragment(Fragment fragment) {
         fragment.setArguments(bundle);
 
@@ -96,6 +108,23 @@ public class MyTours extends Fragment {
             //Log.d(TAG, "onItemClick: " + tours[position].getName());
             bundle.putSerializable("TourObject", tours[position]);
             startFragment(new Viewtour());
+        }
+    }
+
+    public class itemOnSelectedListener implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+            //Log.d(TAG, "onItemSelected: " + position);
+            if (position == 0) {
+                getTours();
+            } else {
+
+            }
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parentView) {
+            // your code here
         }
     }
 }
