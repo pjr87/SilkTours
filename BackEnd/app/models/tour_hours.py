acb7871f2dc7618b662fc2e5c20c0ff37aff8a62
AndrewShidel <andrew@shidel.com>
2017-04-11 17:53:15 -0400

    Adds start and end time to tour_hours

--- BackEnd/app/models/tour_hours.py
+++ BackEnd/app/models/tour_hours.py
@@ -1,5 +1,5 @@
 import datetime
-from sqlalchemy import Column, Integer, ForeignKey, Time
+from sqlalchemy import Column, Integer, ForeignKey, Time, DateTime
 from sqlalchemy.orm import relationship
 from base import Base
 from db_session import session, commitSession
@@ -12,13 +12,17 @@ class TourHours(Base):
     day_of_week = Column(Integer)
     open_time = Column(Time)
     close_time = Column(Time)
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
     # tour = relationship("Tour", foreign_keys=[id_tour])
 
-    def set_props(self, id_tour, day_of_week, open_time, close_time):
+    def set_props(self, id_tour, day_of_week, open_time, close_time, start_time, end_time):
         self.id_tour = id_tour
         self.day_of_week = day_of_week
         self.open_time = open_time
         self.close_time = close_time
+        self.start_time = start_time
+        self.end_time = end_time
         self.tour_hours_id = None
 
     def create(data, id_tour=None):
@@ -31,7 +35,9 @@ class TourHours(Base):
             data.get("tour_id"),
             data.get("day_of_week"),
             data.get("open_time"),
-            data.get("close_time"))
+            data.get("close_time"),
+            data.get("start_time"),
+            data.get("end_time"))
         if id_tour is not None:
             result.id_tour = id_tour
         commitSession(result)
@@ -42,5 +48,7 @@ class TourHours(Base):
             "tour_hours_id": self.tour_hours_id,
             "day_of_week": self.day_of_week,
             "open_time": self.open_time,
-            "close_time": self.lon
+            "close_time": self.lon,
+            "start_time": self.start_time,
+            "end_time": self.end_time
         }
