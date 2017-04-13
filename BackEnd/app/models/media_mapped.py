6f97f4e7be3c0e856ff9e91e1d79c2ac1348b951
Yongqiang Fan <yf322@drexel.edu>
2017-04-12 21:44:52 -0400

    media to sqlachemy

---
+++ BackEnd/app/models/media_mapped.py
@@ -0,0 +1,65 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from base import Base
+from db_session import session, commitSession, createSession, safe_call
+import boto3
+import uuid
+
+class Media(Base):
+    __tablename__ = 'Media'
+    id_media = Column(Integer, primary_key=True)
+    id_tour = Column(Integer)
+    url = Column(String)
+    is_video = Column(Boolean)
+    file_name = Column(String)
+    display_rank = Column(Integer)
+    is_deleted = Column(Boolean)
+
+    VIDEO = ['avi', 'mp4', 'mkv', '3gp', 'wmv']
+
+    def serialize(self):
+        return {
+            "url": self.url,
+            "is_video": self.is_video,
+            "file_name": self.file_name,
+            "display_rank": self.display_rank,
+            "is_deleted": self.is_deleted,
+        }
+
+    def post(self, data):
+        for key in data:
+            setattr(self, key, data[key])
+        createSession()
+        session.add(self)
+        commitSession()
+
+    def isVideo(self, extension):
+        if extension not in self.VIDEO:
+            return 0
+        return 1
+
+    def upload_to_s3(self, file, filename, tourid):
+        resource = boto3.resource('s3')
+        bucket = resource.Bucket('silktours-media')
+        bucket.put_object(Key='tour/' + tourid + '/' + filename, Body=file)
+
+    def upload(self, file, tourid):
+        result = Media()
+        query = session.query(Media).filter(Media.id_tour == tourid)
+        medias = safe_call(query, "all", None)
+        rank = len(medias) + 1
+        print(rank)
+        s = file.filename.split('.')
+        extension = s[-1]
+        filename = uuid.uuid4().hex + "." + extension
+        result.upload_to_s3(file, filename, tourid)
+        url = 'https://s3.amazonaws.com/silktours-media/' + 'tour/' + tourid + '/' + filename
+        is_video = self.isVideo(extension)
+        values = {}
+        values['url'] = url
+        values['is_video'] = is_video
+        values['display_rank'] = rank
+        values['id_tour'] = tourid
+        values['file_name'] = filename
+        result.post(values)
+        return "s"
+
--- BackEnd/run.py
+++ BackEnd/run.py
@@ -12,9 +12,7 @@ from app.models.tour_event_mapped import TourEvent
 from sqlalchemy import func, or_, and_
 import boto3
 from db_session import session, commitSession, safe_call, limiting_query
-
-from app.database_module.controlers import DbController
-from app.s3_module.controlers import S3Controller
+from app.models.media_mapped import Media
 import sys
 
 #outputFile = open('out.log', 'w')
@@ -90,8 +88,6 @@ def notAuthorizedResponse():
     href='http://localhost:5000/login'>here</a> to login.</h1>""", 403
 
 
-db = DbController()
-s3 = S3Controller()
 
 # engine = create_engine('mysql+mysqldb://silktours:32193330@silktoursapp.ctrqouiw79qc.us-east-1.rds.amazonaws.com:3306/silktours', pool_recycle=3600)
 #engine = create_engine(
@@ -281,10 +277,6 @@ def add_stop():
     return "Success"
 
 
-@app.route('/tours', methods=['GET'])
-def get_tour_list():
-    return db.list_tours()
-
 
 @app.route('/tours/<tourid>', methods=['GET'])
 def get_tour(tourid):
@@ -381,17 +373,20 @@ def edit_tourevent(eventid):
     return jsonify(event.serialize())
 
 
-@app.route('/tours/image/<tourid>', methods=['POST'])
+@app.route('/image/<tourid>', methods=['POST'])
 def upload(tourid):
     if not checkLogin():
         return notAuthorizedResponse()
     file = request.files['file']
-    return s3.upload(file, tourid)
+    media = Media()
+    return media.upload(file, tourid)
 
 
-@app.route('/tours/image/<tourid>', methods=['GET'])
+@app.route('/media/<tourid>', methods=['GET'])
 def get_image(tourid):
-    return s3.get_image(tourid)
+    query = session.query(Media).filter(Media.id_tour == tourid)
+    medias = safe_call(query, "all", None)
+    return jsonify([media.serialize() for media in medias])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, threaded=True)
