2412154110b6da49892b5a859acb37f3a0fad0fb
Troy Santry <Troy@Troys-MacBook-Air.local>
2017-05-02 19:12:11 -0400

    ios`

---
+++ IOS/BestOne/BestOne/BestOne/Classes/BackendAPI.swift
@@ -0,0 +1,229 @@
+//
+//  BackendAPI.swift
+//  BestOne
+//
+//  Created by Troy Santry on 5/1/17.
+//  Copyright © 2017 AppsFoundation. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class BackendAPI{
+
+    //OLD window.SERVER_URL = "http://34.197.42.24:5000";
+    
+    /*
+     Every call must use
+     secretAccessKey and identityID - Used with all ajax calls
+     */
+    
+    let SERVER_URL = "http://silk-tours-dev.us-east-1.elasticbeanstalk.com";
+//    func getUser(String id) {
+//        Alamofire.request();
+//    }
+    
+   
+    func getFilteredTours(rating:String, priceMin:Float, priceMax:Float, keywords:String, page:String, page_size:Int) {
+       // return Alamofire.request();
+    }
+    
+    func getTourById(tourId:UInt64) {
+    //return axios.get(SERVER_URL + "/tours/"+tourId);
+    }
+    
+    func getTourEventById(tourId:UInt64){
+    //return axios.get(SERVER_URL + "/tour/"+tourId+"/events");
+    }
+    
+    func putTourEventById(eventid:UInt64, json:String, auth:String) {
+        let url = SERVER_URL + "/tourevents/" + String(eventid);
+//    return axios.put(url, json,
+//        {
+//        headers:{
+//            'Silk-Logins': auth.Logins,
+//            'Silk-Identity-Id': auth.IdentityId
+//        },
+//    });
+    }
+    
+    func putTourEvent(json:String, auth:String) {
+        let url = SERVER_URL + "/tourevents";
+    //return axios.post(url, json,
+//    {
+//    headers:{
+//    'Silk-Logins': auth.Logins,
+//    'Silk-Identity-Id': auth.IdentityId
+//    },
+//    });
+    }
+    
+     func getAllTours() {
+        //return axios.get(SERVER_URL + "/search");
+    }
+    
+    func getUser(id:UInt64){
+        //var url = SERVER_URL + "/users/"+id;
+        //return axios.get(url);
+    }
+    
+    func getUserById(id:UInt64, json:String) {
+        let url = SERVER_URL + "/users/" + String(id);
+//    return axios.get(url, {
+//    headers:{
+//    'Silk-Logins': json.Logins,
+//    'Silk-Identity-Id': json.IdentityId
+//    }
+//    });
+    }
+    
+    func newTour(data:String, auth:String){
+        let url = SERVER_URL + "/tours";
+//    return axios.post(url, data,
+//    {
+//    headers:{
+//    'Silk-Logins': auth.Logins,
+//    'Silk-Identity-Id': auth.IdentityId
+//    },
+//    });
+    }
+    
+//     func newPhoto(data, id, auth){
+//    let url = SERVER_URL + '/media/' + id;
+//    return axios.post(url, data,
+//    {
+//    headers:{
+//    'Silk-Logins': auth.Logins,
+//    'Silk-Identity-Id': auth.IdentityId
+//    },
+//    });
+//    }
+//    
+//     func newTourProfilePhoto(data, id, auth){
+//    let url = SERVER_URL + '/tours/' + id + '/profile';
+//    return axios.put(url, data,
+//    {
+//    headers:{
+//    'Silk-Logins': auth.Logins,
+//    'Silk-Identity-Id': auth.IdentityId
+//    },
+//    });
+//    }
+//    
+//     func newUserProfilePhoto(data, id, auth){
+//    let url = SERVER_URL + '/users/' + id + '/profile';
+//    return axios.put(url, data,
+//    {
+//    headers:{
+//    'Silk-Logins': auth.Logins,
+//    'Silk-Identity-Id': auth.IdentityId
+//    },
+//    });
+//    }
+    
+    
+    /*Yes, send a POST to "/users" to create or a PUT to "/users/<id>" to edit.
+     The arguments should be put in the HTTP request's form, and the JSON structure
+     of the params is the same as the result of "http://34.197.42.24:5000/users/1"
+     (although you only need to supply the values that you want to set).*/
+    func registerNewUser(json: String) {
+    //return axios.post(SERVER_URL + '/users', json);
+    }
+    
+    //func updateExistingUser(id, json, auth) {
+    //let url = SERVER_URL + '/users/' + id;
+//    return axios.put(url, json,
+//    {
+//    headers:{
+//    'Silk-Logins': auth.Logins,
+//    'Silk-Identity-Id': auth.IdentityId
+//    },
+//    });
+   // }
+    
+     //func getUserByEmail(email, json) {
+   // let url = SERVER_URL + '/users/email/' + email;
+//    return axios.get(url, {
+//    headers:{
+//    'Silk-Logins': json.Logins,
+//    'Silk-Identity-Id': json.IdentityId
+//    }
+//    });
+  //  }
+    
+    
+   //  func getMessages(){
+   // var url = 'https://demo5229068.mockable.io/messages';
+    
+ //   return axios.get(url);
+//    }
+    
+    
+//     func postSupportTicket(department, fname, lname, email, textBody) {
+//    var instance = axios.create({
+//    baseURL: 'https://silktoursinc.freshdesk.com/api/v2/tickets',
+//    headers: {'Content-Type': 'application/json'},
+//    auth: { username: '0e7cUf93oqBAib5lwaN6', password: 'x'}
+//    });
+//    
+//    var namee = fname + " "  + lname;
+//    
+//    
+//    var desc = JSON.stringify(textBody);
+//    var desc2 = desc.replace(/&/gm, "&amp;")
+//    .replace(/</gm, "&lt;")
+//    .replace(/>/gm, "&gt;")
+//    .replace(/'/gm, "&#039;").replace(/\\n|\\r\\n|\\r/gm, "<br />");
+//    
+//    
+//    
+//    
+//    var dataa = '{ "description": ' + desc2 + ', "subject": "Silk Contact Form", '+
+//    '"email": "' + email + '", "type": "' + department + '", "priority": 1, "status": 2, "name": "' + namee + '"}';
+//    
+//    
+//    return instance.post('', dataa, instance);
+//    
+//}
+//
+// func getPendingReviewsByUserId(userId, auth){
+//    let url = SERVER_URL + '/pending_reviews/' + userId;
+//    
+//    return axios.get(url,
+//                     {
+//                        headers:{
+//                            'Silk-Logins': auth.Logins,
+//                            'Silk-Identity-Id': auth.IdentityId
+//                        },
+//    });
+//}
+//
+// func postPendingReviewsByRatingComment(json, auth){
+//    let url = SERVER_URL + '/ratings';
+//    
+//    return axios.post(url, json,
+//                      {
+//                        headers:{
+//                            'Silk-Logins': auth.Logins,
+//                            'Silk-Identity-Id': auth.IdentityId
+//                        },
+//    });
+//}
+//
+// func putClearPendingReviewsByEventId(eventId, auth){
+//    let url = SERVER_URL + '/clear_pending_review/' + eventId;
+//    
+//    return axios.put(url,
+//                     {
+//                        headers:{
+//                            'Silk-Logins': auth.Logins,
+//                            'Silk-Identity-Id': auth.IdentityId
+//                        },
+//    });
+//}
+//
+    
+    
+    
+    
+}
