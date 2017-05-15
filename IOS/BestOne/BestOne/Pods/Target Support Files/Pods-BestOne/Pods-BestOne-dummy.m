c15475dd9ee0c09d6bd79761cc8a29976a444369
Troy Santry <Troy@Troys-MacBook-Air.local>
2017-05-14 23:09:24 -0400

    Loaded list of tours from database and deisplayed w/out pics right now

--- IOS/BestOne/BestOne/Pods/Target Support Files/Pods-BestOne/Pods-BestOne-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BestOne : NSObject
-@end
-@implementation PodsDummy_Pods_BestOne
-@end
--- IOS/BestOne/BestOne/Pods/Target Support Files/Pods-BestOneWatchKitExtension/Pods-BestOneWatchKitExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BestOneWatchKitExtension : NSObject
-@end
-@implementation PodsDummy_Pods_BestOneWatchKitExtension
-@end
