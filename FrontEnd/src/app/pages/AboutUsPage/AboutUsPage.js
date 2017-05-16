6abc7bada789bc3298f52cab6af27e268f03a1ff
junhoan0702 <junhoan0702@gmail.com>
2017-05-15 23:13:40 -0400

    Adding header on aboutUs and explore pages.

--- FrontEnd/src/app/pages/AboutUsPage/AboutUsPage.js
+++ FrontEnd/src/app/pages/AboutUsPage/AboutUsPage.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Style from './style.css';
+import abouthero from '../../style/images/abouthero.jpg';
+import aboutus from '../../style/images/aboutus.jpg';
+import {PageTitle, AboutContents} from 'components';
+import Image from 'react-bootstrap/lib/Image';
 
-import {PageTitle, AboutContents} from 'components'
 
 class AboutUsPage extends React.Component{
   render(){
     return(
       <div>
+        <div className = {Style.pane}>
+          <div className= {Style.sideLeft}>
+            <Image className="img-responsive" src={abouthero}/>
+          </div>
+          <div className = {Style.sideRight}>
+            <Image className="img-responsive" src={aboutus}/>
+            <p className = {Style.paneText}>Aenean sollicitudin, erat a elementum</p>
+            <p className = {Style.paneText}>rutrum, neque sem pretium metus, quis</p>
+            <p className = {Style.paneText}>mollis nisl nunc et massa. Vestibulum</p>
+            <p className = {Style.paneText}>sed metus in lorem tristique</p>
+            <p className = {Style.paneText}>ullamcorper id vitae erat. Nulla mollis</p>
+            <p className = {Style.paneText}>sapien sollicitudin lacinia lacinia.</p>
+          </div>
+        </div>
         <br/>
         <br/>
         <PageTitle title= "Mission Statement"/>
